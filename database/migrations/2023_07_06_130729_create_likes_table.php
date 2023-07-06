0b850b29fa6ec36c967fbc12809719237ee62a2d
elexispd <promisedeco24@gmail.com>
2023-07-06 17:06:09 +0100

    add a few more migrations

---
+++ database/migrations/2023_07_06_130729_create_likes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->string("username");
+            $table->string("post_id");
+            $table->timestamps();
+
+            $table->foreign('username')->references('username')->on('users')->onDelete('cascade');
+            $table->foreign('post_id')->references('post_id')->on('posts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+};
