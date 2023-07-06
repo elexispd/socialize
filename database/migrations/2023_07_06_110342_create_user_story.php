463ca0500e364efe02cd2d6f06040f1ea1012e89
elexispd <promisedeco24@gmail.com>
2023-07-06 12:29:09 +0100

    Uploaded framework

---
+++ database/migrations/2023_07_06_110342_create_user_story.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_story', function (Blueprint $table) {
+            $table->id();
+            $table->string("username");
+            $table->string("story_text", 255)->nullable();
+            $table->string("story_photo")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_story');
+    }
+};
