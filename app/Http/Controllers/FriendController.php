0d388e6d30fa6c715747ad7cc30da28b4ccfa87c
elexis <promisedeco24@gmail.com>
2023-09-24 13:37:20 +0100

    added selection for friends of friends quit end

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -15,21 +15,46 @@ class FriendController extends Controller
     public function explore() {
         $loggedInUser = auth()->user();
 
+        $usersNotFriendsWithLoggedInUser = $this->notFriends($loggedInUser);
+        $friendsOfFriends = $this->friendsYouMayKnow($loggedInUser);
 
+        return view('explore.find-friends', compact('usersNotFriendsWithLoggedInUser', 'friendsOfFriends'));
+    }
+
+    function notFriends($loggedInUser) {
         $usersNotFriendsWithLoggedInUser = User::where('id', '!=', $loggedInUser->id)
-
             ->whereNotIn('id', $loggedInUser->friends->pluck('id')) // Exclude friends of the logged-in user
             ->whereNotIn('id', $loggedInUser->friendsWith->pluck('id'))
-
             ->get();
-
-
-
-
-
-
-        return view('explore.find-friends', compact('usersNotFriendsWithLoggedInUser'));
+        return $usersNotFriendsWithLoggedInUser;
     }
+
+    function friendsYouMayKnow($loggedInUser) {
+        // Get the IDs of the logged-in user's friends from both scenarios
+        $loggedInUserFriendIds = array_merge(
+            $loggedInUser->friends->pluck('id')->toArray(),
+            $loggedInUser->friendsWith->pluck('id')->toArray()
+        );
+
+        $friendsOfFriends = User::where(function ($query) use ($loggedInUserFriendIds) {
+            $query->whereHas('friends', function ($subQuery) use ($loggedInUserFriendIds) {
+                $subQuery->whereIn('friend_id', $loggedInUserFriendIds);
+            })
+            ->orWhereHas('friendsWith', function ($subQuery) use ($loggedInUserFriendIds) {
+                $subQuery->whereIn('user_id', $loggedInUserFriendIds);
+            });
+        })
+        ->whereNotIn('id', $loggedInUserFriendIds) // Exclude logged-in user's friends
+        ->where('id', '!=', $loggedInUser->id) // Exclude the logged-in user
+        ->get();
+
+        return $friendsOfFriends;
+    }
+
+
+
+
+
 
 
 
--- resources/views/explore/find-friends.blade.php
+++ resources/views/explore/find-friends.blade.php
@@ -53,61 +53,19 @@
                         <div class="uk-slider-container px-1 py-3">
                             <ul class="uk-slider-items uk-child-width-1-3@m uk-child-width-1-3@s uk-child-width-1-2 uk-grid-small uk-grid">
 
-                                <li>
-                                    <a href="#" class="uk-link-reset">
-                                        <div class="card">
-                                            <img src="assets/images/avatars/avatar-6.jpg" class="h-44 object-cover rounded-t-md shadow-sm w-full">
-                                            <div class="p-4">
-                                                <h4 class="text-base font-semibold mb-1"> James Lewis </h4>
-                                                <a href="#" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-sm text-xs">Add Friends</a>
+                                @foreach ($friendsOfFriends as $friendOfFriends)
+                                    <li>
+                                        <a href="#">
+                                            <div class="card">
+                                                <img src="{{ asset('useravatar/default.jpg') }}" class="h-44 object-cover rounded-t-md shadow-sm w-full">
+                                                <div class="p-4">
+                                                    <h4 class="text-base font-semibold mb-1"> {{ $friendOfFriends->firstname }} {{ $friendOfFriends->lastname }} </h4>
+                                                    <a href="#" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-sm text-xs">Add Friends</a>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <div class="card">
-                                            <img src="assets/images/avatars/avatar-4.jpg" class="h-44 object-cover rounded-t-md shadow-sm w-full">
-                                            <div class="p-4">
-                                                <h4 class="text-base font-semibold mb-1"> Martin Gray </h4>
-                                                <a href="#" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-sm text-xs">Add Friends</a>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <div class="card">
-                                            <img src="assets/images/avatars/avatar-7.jpg" class="h-44 object-cover rounded-t-md shadow-sm w-full">
-                                            <div class="p-4">
-                                                <h4 class="text-base font-semibold mb-1">John Michael </h4>
-                                                <a href="#" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-sm text-xs">Add Friends</a>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <div class="card">
-                                            <img src="assets/images/avatars/avatar-5.jpg" class="h-44 object-cover rounded-t-md shadow-sm w-full">
-                                            <div class="p-4">
-                                                <h4 class="text-base font-semibold mb-1">Jesse Stevens </h4>
-                                                <a href="#" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-sm text-xs">Add Friends</a>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <div class="card">
-                                            <img src="assets/images/avatars/avatar-3.jpg" class="h-44 object-cover rounded-t-md shadow-sm w-full">
-                                            <div class="p-4">
-                                                <h4 class="text-base font-semibold mb-1"> Monroe Parker </h4>
-                                                <a href="#" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-sm text-xs">Add Friends</a>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </li>
+                                        </a>
+                                    </li>
+                                @endforeach
 
                             </ul>
 
